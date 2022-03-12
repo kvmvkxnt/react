48bc0cb198fb4f92cbe45d4af7ae82acd30c7bea
kvmvkxnt <kamron1007dada@gmail.com>
2022-03-12 23:23:17 +0500

    Done with theme switch

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import {Context as ThemeContext, Provider as ThemeProvider} from './Context/Theme';
+import { Provider as ThemeProvider } from './Context/Theme';
 
 ReactDOM.render(
     <React.StrictMode>
